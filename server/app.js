d5276a2c012e99c47290d32d7694d4d8e9032ac4
Alexander Collin <alexandercollin2010@gmail.com>
2016-12-14 16:39:01 -0600

    Got fred to show up in db

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser= require( 'body-parser' );
+var pg= require('pg');
 var urlencodedParser = bodyParser.urlencoded( {extended: false } );
 var port = process.env.PORT || 8080;
 // static folder
 app.use( express.static( 'public' ) );
+
+var connectionString= 'postgres://localhost:5432/Holla-Back-Girl';
 
 // spin up server
 app.listen( port, function(){
@@ -32,12 +35,25 @@ app.get( '/getKoalas', function( req, res ){
 // add koala
 app.post( '/addKoala', urlencodedParser, function( req, res ){
   console.log( 'addKoala route hit' );
+
+  pg.connect(connectionString, function(err, client, done){
+    if(err){
+      console.log(err);
+    } else {
+      console.log('back from db');
+      console.log(typeof req.body.age);
+      client.query('INSERT INTO koala_bears(name, age, sex, ready_for_transfer, notes) values($1, $2, $3, $4, $5)', [req.body.name, req.body.age, req.body.sex, req.body.readyForTransfer, req.body.notes] );
+      done();
+      res.send('hola back girl!!');
+    }
+  });
+
   //assemble object to send
   var objectToSend={
     response: 'from addKoala route'
   }; //end objectToSend
   //send info back to client
-  res.send( objectToSend );
+  //res.send( objectToSend );
 });
 
 // add koala
